c0331b8645526b48598fbe46d0bd7b227d3e4cc6
Edmundo Paulino <60408300+mdmundo@users.noreply.github.com>
2021-08-19 09:13:36 -0300

    icon color

--- src/clients/Table.js
+++ src/clients/Table.js
@@ -1,57 +1,70 @@
 import MUIDataTable from "mui-datatables";
+import { makeStyles } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import EditIcon from "@material-ui/icons/Edit";
 import Grid from "@material-ui/core/Grid";
 import FindInPageIcon from "@material-ui/icons/FindInPage";
 
-const columns = [
-  "Nome",
-  "Empresa",
-  "Cidade",
-  "Estado",
-  {
-    name: "Ações",
-    options: {
-      filter: true,
-      customBodyRender: (value, tableMeta, updateValue) => {
-        return (
-          <Grid
-            container
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="center"
-          >
-            <IconButton color="inherit">
-              <EditIcon />
-            </IconButton>
-            <IconButton color="inherit">
-              <FindInPageIcon />
-            </IconButton>
-          </Grid>
-        );
-      }
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    "&:hover": {
+      color: theme.palette.primary.main
     }
   }
-];
+}));
 
-const data = [
-  ["Joe James", "Test Corp", "Yonkers", "NY", "1"],
-  ["John Walsh", "Test Corp", "Hartford", "CT", "2"],
-  ["Bob Herm", "Test Corp", "Tampa", "FL", "3"],
-  ["James Houston", "Test Corp", "Dallas", "TX", "4"]
-];
+const Table = () => {
+  const classes = useStyles();
 
-const options = {
-  filterType: "checkbox"
+  const columns = [
+    "Nome",
+    "Empresa",
+    "Cidade",
+    "Estado",
+    {
+      name: "Ações",
+      options: {
+        filter: true,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <IconButton className={classes.icon} color="textSecondary">
+                <EditIcon />
+              </IconButton>
+              <IconButton className={classes.icon} color="textSecondary">
+                <FindInPageIcon />
+              </IconButton>
+            </Grid>
+          );
+        }
+      }
+    }
+  ];
+
+  const data = [
+    ["Joe James", "Test Corp", "Yonkers", "NY", "1"],
+    ["John Walsh", "Test Corp", "Hartford", "CT", "2"],
+    ["Bob Herm", "Test Corp", "Tampa", "FL", "3"],
+    ["James Houston", "Test Corp", "Dallas", "TX", "4"]
+  ];
+
+  const options = {
+    filterType: "checkbox"
+  };
+
+  return (
+    <MUIDataTable
+      title={"Lista de Clientes"}
+      data={data}
+      columns={columns}
+      options={options}
+    />
+  );
 };
-
-const Table = () => (
-  <MUIDataTable
-    title={"Lista de Clientes"}
-    data={data}
-    columns={columns}
-    options={options}
-  />
-);
 
 export { Table as default };
