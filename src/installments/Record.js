c745117edb54f508889c4ffd69040e29b132ecc5
Edmundo Paulino <manyymoore@gmail.com>
2021-09-14 11:22:22 -0300

    fix: 🔥 Remove unused dependency

--- src/installments/Record.js
+++ src/installments/Record.js
@@ -1,5 +1,4 @@
 import { forwardRef } from "react";
-import Image from "material-ui-image";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
